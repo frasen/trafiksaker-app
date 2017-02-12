dbf5783c39e3b1c0879751f915e3babccfb42e66
frasen <henrik@frasen.net>
2017-02-12 13:25:21 +0100

    added basic sqlite support - not fully functioning

--- README.md
+++ README.md
@@ -9,12 +9,19 @@ Requires npm (node.js) and an up-to-date version of ruby.
 		1. `brew cask install java`
 		1. `brew install android-sdk`
 		1. `brew install cocoapods`
+1. Add platforms: 
+	1. `ionic add platform ios`
+	1. `ionic add platform browser`
 
 ## Run in browser
+
+Will not work with sqllite.
 
 `ionic serve`
 
 ## Build or emulate
+
+Run with cordova plugins: `ionic run ios`
 
 Emulate: `cordova emulate ios`
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { Geolocation, StatusBar, Splashscreen } from 'ionic-native';
+import { SQLite, Geolocation, StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -15,15 +15,37 @@ export class MyApp {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
+
+
       StatusBar.styleDefault();
       Splashscreen.hide();
+
+
+      /* Geo test */
       Geolocation.getCurrentPosition().then(pos => {
         console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
       });
       let watch = Geolocation.watchPosition().subscribe(pos => {
         console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
-        
+
       });
+
+
+      /* sqlite test (javascript, not Typescript) */
+      let db = new SQLite();
+      db.openDatabase({
+        name: 'data.db',
+        location: 'default' // the location field is required
+      }).then(() => {
+        db.executeSql('select * from developers', {}).then(() => {
+           
+        }, (err) => {
+          console.error('Unable to execute sql: ', err);
+        });
+      }, (err) => {
+        console.error('Unable to open database: ', err);
+      });
+
     });
   }
 }
