a01f14a94f5bfaa48b985dbb376603c225303390
Emil Stenström <em@kth.se>
2017-03-11 17:38:08 +0100

    Remove SQLite. FOREVER.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { NativeAudio, SQLite, StatusBar, Splashscreen } from 'ionic-native';
+import { NativeAudio, StatusBar, Splashscreen } from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -17,7 +17,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
 
-
       StatusBar.styleDefault();
       Splashscreen.hide();
 
@@ -25,22 +24,6 @@ export class MyApp {
         console.log("Sound loaded")
       }, function() {
         console.log("Sound not loaded")
-      });
-
-
-      /* sqlite test (javascript, not Typescript) */
-      let db = new SQLite();
-      db.openDatabase({
-        name: 'data.db',
-        location: 'default' // the location field is required
-      }).then(() => {
-        db.executeSql('select * from developers', {}).then(() => {
-
-        }, (err) => {
-          console.error('Unable to execute sql: ', JSON.stringify(err));
-        });
-      }, (err) => {
-        console.error('Unable to open database: ', JSON.stringify(err));
       });
 
     });
