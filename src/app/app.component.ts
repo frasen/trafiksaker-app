9a189ac2c7bc466aab9039d52f336c84540bd89f
frasen <henrik@frasen.net>
2017-03-11 20:58:14 +0100

    struktur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { NativeAudio, StatusBar, Splashscreen, File} from 'ionic-native';
+import { NativeAudio, StatusBar, Splashscreen, File, BackgroundMode} from 'ionic-native';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { TrafikData } from '../providers/trafik-data';
+import { LocationTracker } from '../providers/location-tracker';
 
 
 @Component({
@@ -13,21 +14,16 @@ export class MyApp {
   rootPage = TabsPage;
 
 
-  constructor(platform: Platform, myTrafikData: TrafikData) {
+  constructor(platform: Platform) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
 
+      // Enable the app to run in the background
+      BackgroundMode.enable();
+
       StatusBar.styleDefault();
       Splashscreen.hide();
-      
-      console.log("Loading trafikdata...");
-      myTrafikData.load().then((data) => {
-        console.log("Loading complete.");
-        console.log("Searching for Trafikelement4: " + myTrafikData.search("Trafikelement4"));
-        console.log("Searching for Trafikelement44: " + myTrafikData.search("Trafikelement44"));
-        console.log("Searching for Elefant: " + myTrafikData.search("Elefant"));
-      });
 
 
       NativeAudio.preloadSimple('uniqueId1', 'farlig-korsning-inbyggt-mic.m4a').then(function(){
@@ -38,4 +34,6 @@ export class MyApp {
 
     });
   }
+
+
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-import { LocationTracker } from '../../providers/location-tracker';
 import { NativeAudio} from 'ionic-native';
+
+import { LocationTracker } from '../../providers/location-tracker';
+import { TrafikData } from '../../providers/trafik-data';
 
 @Component({
   selector: 'page-home',
@@ -10,8 +12,23 @@ import { NativeAudio} from 'ionic-native';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController, public locationTracker: LocationTracker) {
-      
+  public old_lat: number;
+  public old_lng: number;
+  public active = true;
+
+  constructor(public navCtrl: NavController, public locationTracker: LocationTracker, myTrafikData: TrafikData) {
+
+      this.old_lat = 0;
+      this.old_lng = 0;
+      // Load data from file into memory
+      console.log("Loading trafikdata...");
+      myTrafikData.load().then((data) => {
+        console.log("Loading complete.");
+        console.log("Searching for Trafikelement4: " + myTrafikData.search("Trafikelement4"));
+        console.log("Searching for Trafikelement44: " + myTrafikData.search("Trafikelement44"));
+        console.log("Searching for Elefant: " + myTrafikData.search("Elefant"));
+      });
+
   }
 
   playSound(){
@@ -26,9 +43,31 @@ export class HomePage {
 
   start(){
     this.locationTracker.startTracking();
+    this.active = true;
+    while(this.active) {
+      this.sleep(1000);
+      console.log("Running BoomRank")
+      let lat = this.locationTracker.lat;
+      let lng = this.locationTracker.lng;
+      console.log("Lat: " + lat + " Lng: " + lng);
+      console.log("Lat: " + this.old_lat + " Lng: " + this.old_lng);
+
+      this.old_lat = lat;
+      this.old_lng = lat;
+    }
   }
 
   stop(){
+    this.active = false;
     this.locationTracker.stopTracking();
+  }
+
+  sleep(milliseconds) {
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) {
+      if ((new Date().getTime() - start) > milliseconds){
+        break;
+      }
+    }
   }
 }
